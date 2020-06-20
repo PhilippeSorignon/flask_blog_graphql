ba3ed93d91de7524207a0783b2dde002f9678f7b
Philippe Sorignon <philippe.sorignon@protonmail.com>
2020-06-21 00:52:17 +0200

    Added CORS

--- blog/post/views.py
+++ blog/post/views.py
@@ -1,11 +1,14 @@
 from flask import Blueprint
 from flask_graphql import GraphQLView
+from flask_cors import CORS
 
 from blog import admin, db, SecureView
 from blog.post.schema import schema
 from blog.post.models import Post
 
 post = Blueprint('poste', __name__)
+
+CORS(post)
 
 post.add_url_rule(
     '/graphql',
